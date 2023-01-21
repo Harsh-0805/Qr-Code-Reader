f1c6153ad7ba3bfb4ec0fede7cc788b5f5a1674f
shreyasjaiwal <shreyasjaiwal@gmail.com>
2023-01-21 11:51:16 +0530

    new changes

---
+++ src/Pages/QrScanner.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useState } from "react";
+import QrReader from "react-qr-reader";
+import '../styles.css';
+const QrScanner = () => {
+    const [code, setCode] = useState(null);
+    const [precScan, setPrecScan] = useState("");
+    const [selected, setSelected] = useState("environment");
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [data,setData]=useState(null);
+    const [event,setEvent]=useState("");
+    const handleScan = async (scanData) => {
+        console.log(`loaded data data`, scanData);
+        if (scanData && scanData !== "") {
+          console.log(`loaded >>>`, scanData);
+          setData(scanData);
+          // await fetchData({ qr: scanData });
+        }
+      };
+      const handleError = (err) => {
+        console.error(err);
+      };
+      const {id}=JSON.stringify(data)
+      console.log(event);
+  return (
+    <div>
+    <h1 >Scan The QR</h1>
+    <select onChange={(e)=>{setEvent(e.target.value)}}>
+        <option>Event 1</option>
+        <option>Event 2</option>
+    </select>
+    
+        <QrReader
+          facingMode={selected}
+          delay={500}
+          onError={handleError}
+          onScan={handleScan}
+          // chooseDeviceId={()=>selected}
+          style={{ width: "500px", heigth: "500px",margin:"50px"}}
+        />
+      {JSON.stringify(data)}
+      {event}
+    </div>
+  )
+}
+
+export default QrScanner
